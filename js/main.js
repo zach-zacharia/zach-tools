8f8dda81e99cce37c4f0c2f23ecedc020844874d
ZachZacharia <67816122+zach-zacharia@users.noreply.github.com>
2024-07-10 10:41:21 +0700

    Testing dynamic loading

--- js/main.js
+++ js/main.js
@@ -210,3 +210,19 @@
     
 })(jQuery);
 
+function loadPage(page) {
+    fetch(page)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
