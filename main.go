41c5ea3bc9a8a43b551700bdc71b1315fd208946
ZachZacharia <67816122+zach-zacharia@users.noreply.github.com>
2024-07-11 11:47:12 +0700

    a

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
     r := gin.Default()
 
     // Serve static files (CSS, JS, images, etc.)
-    r.Static("/download", "./download*")
+    r.Static("", "./")
 
     // Load HTML templates from the templates directory
     r.LoadHTMLGlob("*.html")
